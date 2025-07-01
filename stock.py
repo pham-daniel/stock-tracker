5acdc20a96fbdf3a9c664ff0fd5cbaddb9ca54c3
Folcenia <danielpham080@gmail.com>
2025-07-01 13:59:25 +1000

    Creating tab initialization function

--- stock.py
+++ stock.py
@@ -42,3 +42,45 @@ class Backend:
         line=dict(color=line_color),
         ))
         return fig
+    
+    @staticmethod
+    def init_tab(ticker,period):
+        interval = {
+            "1d": "5m",
+            "5d": "5m",
+            "1mo": "30m",
+            "6mo": "1d",
+            "1y": "1d",
+            "5y": "7d",
+            "max": "1mo",
+        }
+        period_convert = {
+            "1 day": "1d",
+            "5 days": "5d",
+            "1 month": "1mo",
+            "6 months": "6mo",
+            "1 year": "1y",
+            "5 years": "5y",
+            "Max": "max",    
+        }
+        price, prev_close = Backend.get_ticker_info(ticker)
+        if st.session_state.graph_type == "Line" or st.session_state.refresh_overview:
+            fig = Backend.create_line_chart(ticker,price, prev_close,period_convert[period],interval[period_convert[period]])
+            fig.update_layout(
+                xaxis=dict(type='category',showticklabels=False,showgrid=False,zeroline=False), 
+                yaxis=dict(showticklabels=False,showgrid=False,zeroline=False)
+            )
+
+            if st.session_state.refresh_overview:
+                st.plotly_chart(fig,key=ticker,config={
+                "staticPlot": True
+        })
+            else:
+                st.plotly_chart(fig,key=ticker)
+
+        elif st.session_state.graph_type == "Candlestick":
+            fig = Backend.create_candlestick_chart(ticker,period_convert[period],interval[period_convert[period]])
+            fig.update_layout(
+                xaxis=dict(type='category',showticklabels=False,showgrid=False,zeroline=False)
+            )
+            st.plotly_chart(fig,key=ticker)
